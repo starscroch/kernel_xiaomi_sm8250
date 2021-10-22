c93449408429b83a8e1daf6a952d8a857ba5cb40
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2021-10-22 18:07:47 +0200

    PM: sleep: Pause cpuidle later and resume it earlier during system transitions
    Commit 8651f97bd951 ("PM / cpuidle: System resume hang fix with
    cpuidle") that introduced cpuidle pausing during system suspend
    did that to work around a platform firmware issue causing systems
    to hang during resume if CPUs were allowed to enter idle states
    in the system suspend and resume code paths.
    
    However, pausing cpuidle before the last phase of suspending
    devices is the source of an otherwise arbitrary difference between
    the suspend-to-idle path and other system suspend variants, so it is
    cleaner to do that later, before taking secondary CPUs offline (it
    is still safer to take secondary CPUs offline with cpuidle paused,
    though).
    
    Modify the code accordingly, but in order to avoid code duplication,
    introduce new wrapper functions, pm_sleep_disable_secondary_cpus()
    and pm_sleep_enable_secondary_cpus(), to combine cpuidle_pause()
    and cpuidle_resume(), respectively, with the handling of secondary
    CPUs during system-wide transitions to sleep states.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>
    Tested-by: Ulf Hansson <ulf.hansson@linaro.org>
    
    Signed-off-by: munel <starscroch90@gmail.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -952,7 +952,6 @@ void dpm_resume_early(pm_message_t state)
 void dpm_resume_start(pm_message_t state)
 {
 	dpm_resume_noirq(state);
-	cpuidle_resume();
 	dpm_resume_early(state);
 }
 EXPORT_SYMBOL_GPL(dpm_resume_start);
@@ -1666,11 +1665,8 @@ int dpm_suspend_end(pm_message_t state)
 	if (error)
 		return error;
 
-	cpuidle_pause();
-
 	error = dpm_suspend_noirq(state);
 	if (error) {
-		cpuidle_resume();
 		dpm_resume_early(resume_event(state));
 		return error;
 	}
