76b52359b6505770435e30f05f06fbd6c371c3f3
Sultan Alsawaf <sultan@kerneltoast.com>
2022-04-22 00:07:00 -0700

    kernel: Make perf critical API safe for affining IRQs on RT
    Page allocations can sleep on RT, so kmalloc() is forbidden under raw spin
    locks. Avoid the list node allocation by tacking it onto `struct irq_desc`
    instead, so that we don't need to worry about allocating memory for it
    dynamically at all.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    
    Signed-off-by: munel <starscroch90@gmail.com>

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -23,6 +23,13 @@
 
 #include "internals.h"
 
+static LIST_HEAD(perf_crit_irqs);
+static DEFINE_RAW_SPINLOCK(perf_irqs_lock);
+static int perf_cpu_index = -1;
+static int prime_cpu_index = -1;
+static int lp_cpu_index = -1;
+static bool perf_crit_suspended;
+
 #ifdef CONFIG_IRQ_FORCED_THREADING
 __read_mostly bool force_irqthreads;
 EXPORT_SYMBOL_GPL(force_irqthreads);
@@ -1259,6 +1266,133 @@ setup_irq_thread(struct irqaction *new, unsigned int irq, bool secondary)
 	return 0;
 }
 
+static void affine_one_perf_thread(struct irqaction *action)
+{
+	const struct cpumask *mask;
+
+	if (!action->thread)
+		return;
+
+	if (action->flags & IRQF_PERF_AFFINE)
+		mask = cpu_perf_mask;
+	else if (action->flags & IRQF_PRIME_AFFINE)
+		mask = cpu_prime_mask;
+	else
+		mask = cpu_lp_mask;
+
+	action->thread->flags |= PF_PERF_CRITICAL;
+	set_cpus_allowed_ptr(action->thread, mask);
+}
+
+static void unaffine_one_perf_thread(struct irqaction *action)
+{
+	if (!action->thread)
+		return;
+
+	action->thread->flags &= ~PF_PERF_CRITICAL;
+	set_cpus_allowed_ptr(action->thread, cpu_all_mask);
+}
+
+static void affine_one_perf_irq(struct irq_desc *desc, unsigned int perf_flag)
+{
+	const struct cpumask *mask;
+	int *mask_index;
+	int cpu;
+
+	if (perf_flag & IRQF_PERF_AFFINE) {
+		mask = cpu_perf_mask;
+		mask_index = &perf_cpu_index;
+	} else if (perf_flag & IRQF_PRIME_AFFINE) {
+		mask = cpu_prime_mask;
+		mask_index = &prime_cpu_index;
+	} else {
+		mask = cpu_lp_mask;
+		mask_index = &lp_cpu_index;
+	}
+
+	if (!cpumask_intersects(mask, cpu_online_mask)) {
+		WARN(1, "requested perf CPU is offline for %s\n", desc->name);
+		irq_set_affinity_locked(&desc->irq_data, cpu_online_mask, true);
+		return;
+	}
+
+	/* Balance the performance-critical IRQs across the given CPUs */
+	while (1) {
+		cpu = cpumask_next_and(*mask_index, mask, cpu_online_mask);
+		if (cpu < nr_cpu_ids)
+			break;
+		*mask_index = -1;
+	}
+	irq_set_affinity_locked(&desc->irq_data, cpumask_of(cpu), true);
+
+	*mask_index = cpu;
+}
+
+void setup_perf_irq_locked(struct irq_desc *desc, unsigned int perf_flag)
+{
+	raw_spin_lock(&perf_irqs_lock);
+	list_add(&desc->perf_list, &perf_crit_irqs);
+	affine_one_perf_irq(desc, perf_flag);
+	raw_spin_unlock(&perf_irqs_lock);
+}
+
+void irq_set_perf_affinity(unsigned int irq, unsigned int perf_flag)
+{
+	struct irq_desc *desc = irq_to_desc(irq);
+	unsigned long flags;
+
+	if (!desc)
+		return;
+
+	raw_spin_lock_irqsave(&desc->lock, flags);
+	if (desc->action) {
+		desc->action->flags |= perf_flag;
+		irqd_set(&desc->irq_data, IRQD_PERF_CRITICAL);
+		setup_perf_irq_locked(desc, perf_flag);
+	} else {
+		WARN(1, "perf affine: action not set for IRQ%d\n", irq);
+	}
+	raw_spin_unlock_irqrestore(&desc->lock, flags);
+}
+
+void unaffine_perf_irqs(void)
+{
+	struct irq_desc *desc;
+	unsigned long flags;
+
+	raw_spin_lock_irqsave(&perf_irqs_lock, flags);
+	perf_crit_suspended = true;
+	list_for_each_entry(desc, &perf_crit_irqs, perf_list) {
+		raw_spin_lock(&desc->lock);
+		irq_set_affinity_locked(&desc->irq_data, cpu_all_mask, true);
+		unaffine_one_perf_thread(desc->action);
+		raw_spin_unlock(&desc->lock);
+	}
+	raw_spin_unlock_irqrestore(&perf_irqs_lock, flags);
+}
+
+void reaffine_perf_irqs(bool from_hotplug)
+{
+	struct irq_desc *desc;
+	unsigned long flags;
+
+	raw_spin_lock_irqsave(&perf_irqs_lock, flags);
+	/* Don't allow hotplug to reaffine IRQs when resuming from suspend */
+	if (!from_hotplug || !perf_crit_suspended) {
+		perf_crit_suspended = false;
+		perf_cpu_index = -1;
+		prime_cpu_index = -1;
+		lp_cpu_index = -1;
+		list_for_each_entry(desc, &perf_crit_irqs, perf_list) {
+			raw_spin_lock(&desc->lock);
+			affine_one_perf_irq(desc, desc->action->flags);
+			affine_one_perf_thread(desc->action);
+			raw_spin_unlock(&desc->lock);
+		}
+	}
+	raw_spin_unlock_irqrestore(&perf_irqs_lock, flags);
+}
+
 /*
  * Internal function to register an irqaction - typically used to
  * allocate special interrupts that are part of the architecture.
@@ -1688,6 +1822,12 @@ static struct irqaction *__free_irq(struct irq_desc *desc, void *dev_id)
 		if (action->dev_id == dev_id)
 			break;
 		action_ptr = &action->next;
+	}
+
+	if (irqd_has_set(&desc->irq_data, IRQD_PERF_CRITICAL)) {
+		raw_spin_lock(&perf_irqs_lock);
+		list_del(&desc->perf_list);
+		raw_spin_unlock(&perf_irqs_lock);
 	}
 
 	/* Found it - now remove it from the list of entries: */
