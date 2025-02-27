4ad9aca56d1fc614921d80d26df97f66a31557fb
kvsnr113 <kvsnrprojkt113@gmail.com>
2025-02-28 00:20:34 +0700

    msm/synx: completely remove synx debugging
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>
    
    Signed-off-by: munel <starscroch90@gmail.com>

--- drivers/media/platform/msm/synx/synx.c
+++ drivers/media/platform/msm/synx/synx.c
@@ -13,7 +13,6 @@
 
 #include "synx_api.h"
 #include "synx_util.h"
-#include "synx_debugfs.h"
 
 struct synx_device *synx_dev;
 
@@ -1734,7 +1733,6 @@ static int __init synx_init(void)
 	INIT_LIST_HEAD(&synx_dev->import_list);
 	synx_dev->dma_context = dma_fence_context_alloc(1);
 
-	synx_dev->debugfs_root = init_synx_debug_dir(synx_dev);
 	pr_info("synx device init success\n");
 
 	return 0;
