9a89732c875f82f1a7e1cb18ff4ea30935f31d68
munel <starscroch90@gmail.com>
2026-05-01 14:13:46 +0700

    drivers: disable annoying logs
    Signed-off-by: munel <starscroch90@gmail.com>

--- drivers/power/supply/maxim/ds28e16.c
+++ drivers/power/supply/maxim/ds28e16.c
@@ -32,9 +32,9 @@
 #include <linux/random.h>
 #include <linux/sched.h>
 
-#define ds_info pr_info
+#define ds_info pr_debug
 #define ds_dbg pr_debug
-#define ds_err pr_err
+#define ds_err pr_debug
 #define ds_log pr_debug
 
 struct ds28e16_data {
@@ -101,10 +101,10 @@ static void set_sched_affinity_to_current(void)
 	ret = sched_setaffinity(CURRENT_DS28E16_TASK, cpumask_of(current_cpu));
 	preempt_enable();
 	if (ret) {
-		pr_info("Setting cpu affinity to current cpu failed(%ld) in %s.\n",
+		pr_debug("Setting cpu affinity to current cpu failed(%ld) in %s.\n",
 			ret, __func__);
 	} else {
-		pr_info("Setting cpu affinity to current cpu(%d) in %s.\n",
+		pr_debug("Setting cpu affinity to current cpu(%d) in %s.\n",
 			current_cpu, __func__);
 	}
 }
@@ -117,10 +117,10 @@ static void set_sched_affinity_to_all(void)
 	cpumask_setall(&dstp);
 	ret = sched_setaffinity(CURRENT_DS28E16_TASK, &dstp);
 	if (ret) {
-		pr_info("Setting cpu affinity to all valid cpus failed(%ld) in %s.\n",
+		pr_debug("Setting cpu affinity to all valid cpus failed(%ld) in %s.\n",
 			ret, __func__);
 	} else {
-		pr_info("Setting cpu affinity to all valid cpus in %s.\n",
+		pr_debug("Setting cpu affinity to all valid cpus in %s.\n",
 			__func__);
 	}
 }
--- drivers/power/supply/qcom/qpnp-smb5-dagu.c
+++ drivers/power/supply/qcom/qpnp-smb5-dagu.c
@@ -1932,7 +1932,7 @@ static int smb5_usb_port_get_prop(struct power_supply *psy,
 		rc = smblib_get_prop_input_current_settled(chg, val);
 		break;
 	default:
-		pr_err_ratelimited("Get prop %d is not supported in pc_port\n",
+		pr_debug("Get prop %d is not supported in pc_port\n",
 				psp);
 		return -EINVAL;
 	}
@@ -1953,7 +1953,7 @@ static int smb5_usb_port_set_prop(struct power_supply *psy,
 
 	switch (psp) {
 	default:
-		pr_err_ratelimited("Set prop %d is not supported in pc_port\n",
+		pr_debug("Set prop %d is not supported in pc_port\n",
 				psp);
 		rc = -EINVAL;
 		break;
@@ -3225,7 +3225,7 @@ static int smb5_batt_get_prop(struct power_supply *psy,
 		val->intval = chg->smartBatVal;
 		break;
 	default:
-		pr_err("batt power supply prop %d not supported\n", psp);
+		pr_debug("batt power supply prop %d not supported\n", psp);
 		return -EINVAL;
 	}
 
