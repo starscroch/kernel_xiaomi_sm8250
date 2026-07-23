7024ad0e0a9ea01253b6a5e5255503dc9364f16b
munel <starscroch90@gmail.com>
2026-07-23 14:33:03 +0700

    binder: checkout from android13-5.10-lts
    source: https://android.googlesource.com/kernel/common/+/refs/heads/android13-5.10-lts/drivers/android/
    
    last commit head: 50b3a256e550e98ff6a49d32e77fb67998707961
    
    Signed-off-by: munel <starscroch90@gmail.com>

--- include/linux/fdtable.h
+++ include/linux/fdtable.h
@@ -22,6 +22,7 @@
  * as this is the granularity returned by copy_fdset().
  */
 #define NR_OPEN_DEFAULT BITS_PER_LONG
+#define NR_OPEN_MAX ~0U
 
 struct fdtable {
 	unsigned int max_fds;
@@ -79,7 +80,7 @@ struct dentry;
 /*
  * The caller must ensure that fd table isn't shared or hold rcu or file lock
  */
-static inline struct file *files_lookup_fd_raw(struct files_struct *files, unsigned int fd)
+static inline struct file *__fcheck_files(struct files_struct *files, unsigned int fd)
 {
 	struct fdtable *fdt = rcu_dereference_raw(files->fdt);
 
@@ -94,13 +95,21 @@ static inline struct file *files_lookup_fd_rcu(struct files_struct *files, unsig
 {
 	RCU_LOCKDEP_WARN(!rcu_read_lock_held(),
 			   "suspicious rcu_dereference_check() usage");
-	return files_lookup_fd_raw(files, fd);
+	return __fcheck_files(files, fd);
+}
+
+static inline struct file *fcheck_files(struct files_struct *files, unsigned int fd)
+{
+	RCU_LOCKDEP_WARN(!rcu_read_lock_held() &&
+			   !lockdep_is_held(&files->file_lock),
+			   "suspicious rcu_dereference_check() usage");
+	return __fcheck_files(files, fd);
 }
 
 /*
  * Check whether the specified fd has an open file.
  */
-#define fcheck(fd)	files_lookup_fd_rcu(current->files, fd)
+#define fcheck(fd)	fcheck_files(current->files, fd)
 struct file *task_lookup_next_fd_rcu(struct task_struct *task, unsigned int *fd);
 
 struct task_struct;
@@ -109,10 +118,7 @@ struct files_struct *get_files_struct(struct task_struct *);
 void put_files_struct(struct files_struct *fs);
 void reset_files_struct(struct files_struct *);
 int unshare_files(struct files_struct **);
-struct fd_range {
-	unsigned int from, to;
-};
-struct files_struct *dup_fd(struct files_struct *, struct fd_range *) __latent_entropy;
+struct files_struct *dup_fd(struct files_struct *, unsigned, int *) __latent_entropy;
 void do_close_on_exec(struct files_struct *);
 int iterate_fd(struct files_struct *, unsigned,
 		int (*)(const void *, struct file *, unsigned),
@@ -125,6 +131,9 @@ extern void __fd_install(struct files_struct *files,
 extern int __close_fd(struct files_struct *files,
 		      unsigned int fd);
 extern int __close_range(unsigned int fd, unsigned int max_fd, unsigned int flags);
+extern int close_fd_get_file(unsigned int fd, struct file **res);
+extern int unshare_fd(unsigned long unshare_flags, unsigned int max_fds,
+		      struct files_struct **new_fdp);
 
 extern struct kmem_cache *files_cachep;
 
